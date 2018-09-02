959d713fa8b055681b1daeba957825d738047e46
OskarHulter <stresslessness91@gmail.com>
2018-09-02 19:29:26 +0200

    Tried copying the public folder from my portfolio to see if it manages.

---
+++ path----5057d395409fe43cfe68.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{168:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Oskar Hulter"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----5057d395409fe43cfe68.js.map
