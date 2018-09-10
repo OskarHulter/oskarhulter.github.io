6ff3b437e0ca9e88dcca937c99febc55c532202b
OskarHulter <stresslessness91@gmail.com>
2018-09-10 19:34:52 +0200

    added newest update with better font and mobile UX.

---
+++ component---src-pages-404-js-094879b5591decb87f9c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{149:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-094879b5591decb87f9c.js.map
